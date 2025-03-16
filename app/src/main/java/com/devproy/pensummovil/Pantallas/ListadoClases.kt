688e6f8407cfe0b82bdce37cdfae4cd055dca050
RonyMoncada <rfmoncadag@unicah.edu>
2025-03-16 16:24:41 -0600

    Pantalla de Historial Grafico y Listado de Clases

--- app/src/main/java/com/devproy/pensummovil/Pantallas/ListadoClases.kt
+++ app/src/main/java/com/devproy/pensummovil/Pantallas/ListadoClases.kt
@@ -1,8 +1,36 @@
 package com.devproy.pensummovil.Pantallas
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.*
 
-import androidx.compose.runtime.Composable
+import com.devproy.pensummovil.Navigation.MyTopAppBar
+import com.devproy.pensummovil.ViewModels.AprobadoViewModel
+import com.devproy.pensummovil.ui.theme.VerdeUnicah
 
 @Composable
-fun ListadoClases(){
+fun ListadoClases(viewModel: AprobadoViewModel) {
 
+    Scaffold(
+        topBar = { MyTopAppBar(title = "Listado de clases") }
+    ) { innerPadding ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            if (viewModel.isChecked.value) {
+                Text("aprobada", fontSize = 20.sp, color = VerdeUnicah)
+            } else {
+                Text("no hay clases aprobadas", fontSize = 20.sp, color = Color.Red)
+            }
+
+        }
+
+    }
 }
